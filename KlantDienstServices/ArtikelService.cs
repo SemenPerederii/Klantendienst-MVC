9d368a108b66ce07685f02df4e3bffa9b26c30ee
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-01-19 15:42:23 +0100

    ArtikelService aangemaakt en uitgewerkt met GetAlleArtikelen

---
+++ KlantDienstServices/ArtikelService.cs
@@ -0,0 +1,24 @@
+﻿using KlantenDienstData.Models;
+using KlantenDienstData.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlantenDienstServices
+{
+    public class ArtikelService
+    {
+        private readonly IArtikelRepository _artikelRepository;
+        public ArtikelService(IArtikelRepository artikelRepository)
+        {
+            _artikelRepository = artikelRepository;
+        }
+
+        public async Task<IEnumerable<Artikel>> GetAllArtikelenAsync()
+        {
+            return await _artikelRepository.GetAllArtikelenAsync();
+        }
+    }
+}
