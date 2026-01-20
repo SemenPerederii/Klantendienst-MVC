27f17a2f0ef12fdf4241fb8392a2ea58eae512d7
SemenPerederii <semen.perederii@vdabcampus.be>
2026-01-20 10:11:15 +0100

    categorieRepo, categorieService (Interfaces)

---
+++ KlantDienstServices/CategorieService.cs
@@ -0,0 +1,25 @@
+﻿using KlantenDienstData.Models;
+using KlantenDienstData.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlantenDienstServices
+{
+    public class CategorieService : ICategorieService
+    {
+        private readonly ICategorieRepository _repositoryCategorie;
+
+        public CategorieService(ICategorieRepository repository)
+        {
+            _repositoryCategorie = repository;
+        }
+
+        public async Task<List<Categorie>> GetAllCategorie()
+        {
+            return await _repositoryCategorie.GetAll();
+        }
+    }
+}
---
+++ KlantDienstServices/ICategorieService.cs
@@ -0,0 +1,14 @@
+﻿using KlantenDienstData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlantenDienstServices
+{
+    public interface ICategorieService
+    {
+        Task<List<Categorie>> GetAllCategorie();
+    }
+}
