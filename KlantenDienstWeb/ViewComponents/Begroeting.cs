997a76f8e9de7f49f6fc98db71829568d773f42d
SelenaBoeykens <mg.maanie@gmail.com>
2026-01-21 10:03:38 +0100

    begroeting viewcomponent

---
+++ KlantenDienstWeb/ViewComponents/Begroeting.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KlantenDienstWeb.ViewComponents
+{
+    public class Begroeting : ViewComponent
+    {
+        public IViewComponentResult Invoke(string plaats)
+        {
+            var sessionVariabeleNaam = HttpContext.Session.GetString("Voornaam");
+            if (string.IsNullOrEmpty(sessionVariabeleNaam))
+                return View((object)"Naam niet gevonden");
+            return View((object)$"Welkom {sessionVariabeleNaam}!");
+        }
+    }
+}
