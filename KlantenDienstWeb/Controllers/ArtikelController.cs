0117b9b30b6faf84415b740f2783ee71f5778e95
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-01-21 13:43:40 +0100

    CategorieService in ArtikelController geinjecteerd

--- KlantenDienstWeb/Controllers/ArtikelController.cs
+++ KlantenDienstWeb/Controllers/ArtikelController.cs
@@ -7,15 +7,18 @@ namespace KlantenDienstWeb.Controllers
     public class ArtikelController : Controller
     {
         private readonly ArtikelService _artikelService;
-        public ArtikelController(ArtikelService artikelService)
+        private readonly ICategorieService _categorieService;
+        public ArtikelController(ArtikelService artikelService, ICategorieService categorieService)
         {
             _artikelService = artikelService;
+            _categorieService = categorieService;
         }
         public async Task<IActionResult> IndexAsync()
         {
             var artikelVM = new ArtikelViewModel()
             {
                 Artikelen = await _artikelService.GetAllArtikelenAsync(),
+                Categorieën = await _categorieService.GetAllCategorieAsync()
             };
             return View(artikelVM);
         }
