4517a93b628162f747d154b90cfdfd6fa5c73fb7
Putri Heij <Putri.Heij@vdabcampus.be>
2026-01-21 15:50:54 +0100

    icategorieservice geupdated

--- KlantDienstServices/ICategorieService.cs
+++ KlantDienstServices/ICategorieService.cs
@@ -1,18 +1,9 @@
-﻿//using KlantenDienstData.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace KlantenDienstServices
-//{
-//    public interface ICategorieService
-//    {
-//        Task<List<Categorie>> GetAllCategorieAsync();
-//        List<Categorie> BuildTree(List<Categorie> allCategories);
-//    }
-//}
+﻿using KlantenDienstData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace KlantenDienstServices
 {
