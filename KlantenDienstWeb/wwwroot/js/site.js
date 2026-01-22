0c793251c350c4d6f1dbb32f26e82a71cce53ab1
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-01-22 11:59:30 +0100

    View aangepast, Partial view toegevoegd, JS functionaliteit toegevoegd.

--- KlantenDienstWeb/wwwroot/js/site.js
+++ KlantenDienstWeb/wwwroot/js/site.js
@@ -1,21 +1,12 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-"use strict";
-
-<script>
-  // Voorkom dat de dropdown sluit bij klikken binnen de menu
-    document.addEventListener('click', function (e) {
+﻿document.addEventListener('pointerdown', function (e) {
     const menu = e.target.closest('.dropdown-menu');
     if (menu) e.stopPropagation();
-  });
+}, true);
 
-    // Optioneel: als je op de toggle klikt, ook niet laten submitten/closingen
-    document.addEventListener('click', function (e) {
-    const toggle = e.target.closest('.cat-toggle');
-    if (toggle) e.preventDefault();
-  });
-</script>
+document.addEventListener('click', function (e) {
+    const menu = e.target.closest('.dropdown-menu');
+    if (menu) e.stopPropagation();
+}, true);
+
+
 
