f053ffb609215087c8b83b63038284b121da7f83
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-01-27 14:54:06 +0100

    ErrorMessagehandeling aangepast dmv Login DTO

---
+++ KlantDienstServices/DTO's/LogInInfoDTO.cs
@@ -0,0 +1,25 @@
+﻿using KlantenDienstData.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlantenDienstServices.DTO_s
+{
+    public class LogInInfoDTO
+    {
+        [Display(Name = "Emailadres")]
+        [Required(ErrorMessage = "Verplicht")]
+        [DataType(DataType.EmailAddress)]
+        public string Emailadres { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Verplicht")]
+        [DataType(DataType.Password)]
+        public string Paswoord { get; set; } = string.Empty;
+        public PersoneelslidAccount? GevondenAccount { get; set; }
+
+        public string? ErrorMessage { get; set; } = string.Empty;
+    }
+}
