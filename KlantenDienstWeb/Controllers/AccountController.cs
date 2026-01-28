f1f657525267884aec8be46393ac8e642ccb7ed6
Mitch Lenaerts <lenaerts.mitch@gmail.com>
2026-01-28 10:49:14 +0100

    Controller actie voor Passwoord wijzigen aangemaakt (Get)

---
+++ KlantenDienstWeb/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using KlantenDienstServices;
+using KlantenDienstWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KlantenDienstWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AccountService _accountService;
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> WijzigWachtwoord()
+        {
+            var sessionVariabel = HttpContext.Session.GetInt32("PersoneelslidId");
+
+            var account = await _accountService.GetPersoneelslidById(sessionVariabel!.Value);
+
+            var accountVM = new PaswoordWijzigenVM
+            {
+                Account = account!,
+                Emailadres = account!.PersoneelslidAccount.Emailadres
+            };
+            return View(accountVM);
+        }
+    }
+}
---
+++ KlantenDienstWeb/Models/PaswoordWijzigenVM.cs
@@ -0,0 +1,13 @@
+﻿using KlantenDienstData.Models;
+
+namespace KlantenDienstWeb.Models
+{
+    public class PaswoordWijzigenVM
+    {
+        public Personeelslid Account { get; set; }
+        public string Emailadres { get; set; } = string.Empty;
+        public string HuidigPaswoord { get; set; } = string.Empty;
+        public string NieuwPaswoord { get; set; } = string.Empty;
+        public string BevestigNieuwPaswoord { get; set; } = string.Empty;
+    }
+}
