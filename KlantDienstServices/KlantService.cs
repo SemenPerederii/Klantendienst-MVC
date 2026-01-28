b793792612bf52105fc5495dcad9da17ba7f9286
SemenPerederii <semen.perederii@vdabcampus.be>
2026-01-28 14:19:56 +0100

    view, controller, service

---
+++ KlantDienstServices/KlantService.cs
@@ -0,0 +1,35 @@
+﻿using KlantenDienstData.Models;
+using KlantenDienstData.Repositories;
+using KlantenDienstWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlantenDienstServices
+{
+    public class KlantService : IKlantService
+    {
+        private readonly IKlantRepository _repositoryKlant;
+        public KlantService(IKlantRepository repository)
+        {
+            _repositoryKlant = repository;
+        }
+        public async Task<IEnumerable<KlantOverzichtViewModel>> GetAllKlantenAsync()
+        {
+            var klanten = await _repositoryKlant.GetAllKlantenAsync();
+
+            return klanten.Select(k => new KlantOverzichtViewModel
+            {
+                KlantId = k.KlantId,
+                Naam = k.Natuurlijkepersonen != null
+                    ? $"{k.Natuurlijkepersonen.Voornaam} {k.Natuurlijkepersonen.Familienaam}"
+                    : k.Rechtspersonen!.Naam,
+                Type = k.Natuurlijkepersonen != null
+                    ? "Persoon"
+                    : "Bedrijf"
+            });
+        }
+    }
+}
