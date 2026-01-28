d18ef2d785102de5c90cbd7da76e48f5f3643480
Putri Heij <Putri.Heij@vdabcampus.be>
2026-01-28 10:21:02 +0100

    aanpassing nullable parameters

--- KlantDienstServices/ArtikelService.cs
+++ KlantDienstServices/ArtikelService.cs
@@ -1,11 +1,6 @@
 ﻿using KlantenDienstData.Models;
 using KlantenDienstData.Repositories;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KlantenDienstServices
 {
--- KlantenDienstData/Repositories/SqlArtikelRepository.cs
+++ KlantenDienstData/Repositories/SqlArtikelRepository.cs
@@ -1,11 +1,5 @@
 ﻿using KlantenDienstData.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KlantenDienstData.Repositories
 {
